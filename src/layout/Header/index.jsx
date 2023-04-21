5d08369cca0132c157a0a0b70e37863a7940b99b
ArtemD <artemdanilow95@gmail.com>
2023-04-21 15:19:17 +0200

    Create Hero component

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const counter = 0;
 
   return (
-    <header className="p-2 max-w-5xl mx-auto">
+    <header className="px-2 mt-2 max-w-5xl mx-auto">
       <div className="bg-orange-200 p-4 rounded-lg flex justify-between">
         <div className="">
           <h1 className="inline-block font-bold text-white text-center py-2 px-4 bg-orange-500 rounded-lg">
--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Hero from "../components/Hero";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <main className="w-full max-w-5xl mx-auto px-2 mt-2">
+      <Hero />
+    </main>
+  );
 };
 
 export default Home;
