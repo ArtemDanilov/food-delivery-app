c7ddc56d74a3c43f2d416f1c7500efb083b1cac4
ArtemD <artemdanilow95@gmail.com>
2023-04-24 12:53:36 +0200

    Create Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ children }) => {
+  return (
+    <button className="text-gray-600 text-base bg-white py-1 px-4 border border-gray-600 rounded-md hover:bg-gray-100">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
