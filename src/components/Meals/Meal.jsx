551fc1bdd4556fceaf2cc5c44011ce7cd5f053ab
ArtemD <artemdanilow95@gmail.com>
2023-04-24 12:54:39 +0200

    Add Button to Meal component

--- src/components/Meals/Meal.jsx
+++ src/components/Meals/Meal.jsx
@@ -5,10 +5,11 @@ import Star from "../../svg/Star";
 import Money from "../../svg/Money";
 
 import "./style.scss";
+import Button from "../Button";
 
 const Meal = (props) => {
   return (
-    <div className="meal w-full max-w-[30rem] overflow-hidden bg-white border border-gray-300 rounded-lg hover:bg-gray-100 sm:flex sm:max-w-none">
+    <div className="meal w-full max-w-[30rem] overflow-hidden bg-white border border-gray-300 rounded-lg sm:flex sm:max-w-none">
       <img
         src={props.image}
         alt={props.title}
@@ -16,32 +17,37 @@ const Meal = (props) => {
         height="220"
         className="w-full h-full max-h-36 object-cover object-center sm:max-w-[18.75rem]"
       />
-      <div className="p-2">
-        <h3 className="font-bold text-xl mb-2 sm:text-2xl sm:mb-3">
-          {props.name}
-        </h3>
+      <div className="w-full p-2 flex justify-between items-end">
+        <div>
+          <h3 className="font-bold text-xl mb-2 sm:text-2xl sm:mb-3">
+            {props.name}
+          </h3>
 
-        <ul className="list space-y-0.5 sm:space-y-1">
-          <li>
-            <span>
-              <Money width="20" height="20" />
-            </span>
-            {props.currency}
-            {props.price}
-          </li>
-          <li>
-            <span>
-              <Star width="20" height="20" />
-            </span>
-            {props.rating}
-          </li>
-          <li>
-            <span>
-              <ForkAndSpoon width="20" height="20" />
-            </span>
-            {props.cuisine}
-          </li>
-        </ul>
+          <ul className="list space-y-0.5 sm:space-y-1">
+            <li>
+              <span>
+                <Money width="20" height="20" />
+              </span>
+              {props.currency}
+              {props.price}
+            </li>
+            <li>
+              <span>
+                <Star width="20" height="20" />
+              </span>
+              {props.rating}
+            </li>
+            <li>
+              <span>
+                <ForkAndSpoon width="20" height="20" />
+              </span>
+              {props.cuisine}
+            </li>
+          </ul>
+        </div>
+        <div className="p-2">
+          <Button>+ Add</Button>
+        </div>
       </div>
     </div>
   );
